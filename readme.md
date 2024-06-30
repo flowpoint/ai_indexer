263875ec8ea3be46e45c0438173820f3e338f8ca
flowpoint <flowpoint@protonmail.com>
2024-06-30 15:05:54 +0200

    clean up code

--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 # aigrep
 
+requires pdftotext command to be available
+requires lmdb
+
 QUERY='codellama'
 
 ls ~/Documents/*.pdf | ai_indexer - $QUERY
--- src/ai_indexer/cli.py
+++ src/ai_indexer/cli.py
@@ -1,67 +1,35 @@
+import torch
 import torch.nn.functional as F
-
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
-
-
-''' prompt encoding:
-Here are some rules of thumb:
-
-    Use "query: " and "passage: " correspondingly for asymmetric tasks such as passage retrieval in open QA, ad-hoc information retrieval.
-
-    Use "query: " prefix for symmetric tasks such as semantic similarity, paraphrase retrieval.
-
-    Use "query: " prefix if you want to use embeddings as features, such as linear probing classification, clustering.
-'''    
-
-# max ctx len is 512 for large-unsupervised
-
-
-def average_pool(last_hidden_states: Tensor,
-                 attention_mask: Tensor) -> Tensor:
-    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
-
-
-'''
-# Each input text should start with "query: " or "passage: ".
-# For tasks other than retrieval, you can simply use the "query: " prefix.
-input_texts = ['query: how much protein should a female eat',
-               'query: summit define',
-               "passage: As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
-               "passage: Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments."]
-
-tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-small-v2')
-model = AutoModel.from_pretrained('intfloat/e5-small-v2')
-
-# Tokenize the input texts
-batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
-
-outputs = model(**batch_dict)
-embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-print(embeddings)
-'''
-
-'''
-# normalize embeddings
-embeddings = F.normalize(embeddings, p=2, dim=1)
-scores = (embeddings[:2] @ embeddings[2:].T) * 100
-print(scores.tolist())
-'''
 
 import click
 from pathlib import Path
 import subprocess
 import lmdb
 import numpy as np
-import faiss                   # make faiss available
+import faiss
 import io
 import json
-import torch
 from more_itertools import chunked, flatten
 
+model_name = 'intfloat/e5-small-v2'
+db_path = './db.lmdb'
 model = None
 tokenizer = None
+
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
+
+
+# max ctx len is 512 for large-unsupervised
+def average_pool(last_hidden_states: Tensor,
+                 attention_mask: Tensor) -> Tensor:
+    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
 
 def format_text(texts, type_):
     if type_ == 'passage':
@@ -71,24 +39,15 @@ def format_text(texts, type_):
     else:
         raise RuntimeError(f'unkown format {type_}')
     
-if torch.cuda.is_available():
-    device = 'cuda'
-else:
-    device = 'cpu'
 
 def embed_batch(texts, type_):
     global model
     global tokenizer
     if model is None:
-        model = AutoModel.from_pretrained('intfloat/e5-small-v2').to(device)
+        model = AutoModel.from_pretrained(model_name).to(device)
     if tokenizer is None:
-        tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-small-v2')
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    #bs = 1
-    #embeddings = []
-
-    # batch across text sizes
-    #for batch in chunked(texts, bs):
     with torch.no_grad():
         batch = texts
         input_texts = format_text(batch, type_)
@@ -97,19 +56,12 @@ def embed_batch(texts, type_):
 
         outputs = model(**batch_dict)
         eee = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-        #embeddings.append(eee)
 
-        #return [x.float().to('cpu').detach().numpy() for x in flatten(eee)]
         return eee.float().to('cpu').detach().numpy() 
 
-
-
-def search_2(index, query, topk=5):
+def search_faiss(index, query, topk=5):
     qemb = np.array(embed_batch([query], 'query'))
-    #qemb = np.array(embed_batch([query], 'passage'))
-    #print(qemb.shape)
-    qembt = qemb
-    dist, n_idx = index.search(qembt, topk)
+    dist, n_idx = index.search(qemb, topk)
     return n_idx
 
 def file_to_text_splits(fp):
@@ -135,17 +87,13 @@ def build_index_embeds(files: list[str]) -> list[np.array]:
 
 
 def build_index(vecs):
-    index = faiss.IndexFlatL2(384)   # build the index
+    index = faiss.IndexFlatL2(384)
     for v in vecs:
         index.add(v)
     return index
 
 
 
-@click.group()
-@click.pass_context
-def cli(ctx):
-    pass
 
 
 # how much to fill they keys
@@ -153,14 +101,9 @@ def cli(ctx):
 fill_depth = 32
 max_dbsize = int(1e12)
 
-@cli.command()
-@click.argument('query', type=str)
-@click.pass_context
-def search(ctx, query):
-    #env = lmdb.open('db.lmdb', map_size=max_dbsize)
+def search_query(query):
     global max_dbsize
-    env = lmdb.open('db.lmdb', map_size=max_dbsize)
-    print(env.stat())
+    env = lmdb.open(db_path, map_size=max_dbsize)
 
     vecs = []
     with env.begin(buffers=True) as txn:
@@ -178,8 +121,9 @@ def search(ctx, query):
             vecs.append(np.expand_dims(v, 0))
 
     index = build_index(vecs)
-    search_batch = search_2(index, query)
+    search_batch = search_faiss(index, query)
 
+    results = []
     for neighs in search_batch:
         for nidx, n in enumerate(neighs):
             with env.begin(buffers=True) as txn:
@@ -189,7 +133,10 @@ def search(ctx, query):
                 record = json.loads(s)
                 filename = record['fname']
                 t = record['txt']
-                print(f'{nidx}: {n} {filename} {t}')
+                results.append([nidx, n, filename, t])
+
+    return results
+
 
 def stream_files(fp):
     while True:
@@ -201,7 +148,7 @@ def stream_files(fp):
 
 
 
-def index2(pdf_files):
+def index_files(pdf_files):
     global max_dbsize
     env = lmdb.open('db.lmdb', map_size=max_dbsize)
     fst = stream_files(pdf_files)
@@ -218,16 +165,59 @@ def index2(pdf_files):
             txn.put(k,vall)
             print(f)
 
+@click.group()
+@click.pass_context
+def cli(ctx):
+    pass
+
+@cli.command()
+@click.argument('query', type=str)
+@click.pass_context
+def search(ctx, query):
+    for res in search_query(query):
+        nidx, n, filename, t = res
+        print(f'{nidx}: {n} {filename} {t}')
 
 @cli.command()
 @click.argument('pdf_files', type=click.File('r'))
 @click.pass_context
 def index(ctx, pdf_files):
-    index2(pdf_files)
+    index_files(pdf_files)
+
 
 if __name__ == '__main__':
     cli()
 
 
-# needs click transformers torch
-# and pdftotext cli command
+# huggingface usage docs as hint
+''' prompt encoding:
+Here are some rules of thumb:
+
+    Use "query: " and "passage: " correspondingly for asymmetric tasks such as passage retrieval in open QA, ad-hoc information retrieval.
+
+    Use "query: " prefix for symmetric tasks such as semantic similarity, paraphrase retrieval.
+
+    Use "query: " prefix if you want to use embeddings as features, such as linear probing classification, clustering.
+
+# Each input text should start with "query: " or "passage: ".
+# For tasks other than retrieval, you can simply use the "query: " prefix.
+input_texts = ['query: how much protein should a female eat',
+               'query: summit define',
+               "passage: As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
+               "passage: Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments."]
+
+tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-small-v2')
+model = AutoModel.from_pretrained('intfloat/e5-small-v2')
+
+# Tokenize the input texts
+batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+
+outputs = model(**batch_dict)
+embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+print(embeddings)
+
+# normalize embeddings
+embeddings = F.normalize(embeddings, p=2, dim=1)
+scores = (embeddings[:2] @ embeddings[2:].T) * 100
+print(scores.tolist())
+'''
